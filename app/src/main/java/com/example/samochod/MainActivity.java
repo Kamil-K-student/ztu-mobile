747154c9ad598d42f4ed8d087498c17aba737b2e
Kamil Kowalski <kamil_kowalski1991@wp.pl>
2022-06-23 00:34:37 +0200

    added activity_welcome.xml and changed MainActivity - biometric authorisation

--- app/src/main/java/com/example/samochod/MainActivity.java
+++ app/src/main/java/com/example/samochod/MainActivity.java
@@ -1,32 +1,82 @@
 package com.example.samochod;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.biometric.*;
+import androidx.core.content.ContextCompat;
 import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+
+import static androidx.biometric.BiometricManager.Authenticators.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        goToNext();
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
-            public void onClick(View v) {
-                openActivityList();
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                                "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!" , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
             }
         });
 
+        BiometricPrompt.PromptInfo.Builder promptInfo = promptInfo();
+        promptInfo.setNegativeButtonText("Cancel");
+        biometricPrompt.authenticate(promptInfo.build());
+
     }
-    public void openActivityList(){
-        Intent intent = new Intent(this, ListCar.class);
-        startActivity(intent);
+
+    BiometricPrompt.PromptInfo.Builder promptInfo()
+    {
+        return new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login")
+                .setSubtitle("Log in using your biometric credential");
     }
+
+    void goToNext()
+    {
+        BiometricManager manager = BiometricManager.from(this);
+        switch (manager.canAuthenticate(BIOMETRIC_WEAK | BIOMETRIC_STRONG))
+        {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Intent intent = new Intent(this, ListCar.class);
+                startActivity(intent);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                break;
+            default:
+
+        }
+
+    }
+
 }
