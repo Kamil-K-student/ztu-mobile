38cda81c452140aa300daa88314cdaa81504e168
Kamil Kowalski <kamil_kowalski1991@wp.pl>
2022-06-27 14:33:35 +0200

    activity_details new button added and AddCarActivity and activity_add_car added. Methods should be implemented in AddCarActivity - it is empty.

--- app/src/main/java/com/example/samochod/Car.java
+++ app/src/main/java/com/example/samochod/Car.java
@@ -29,6 +29,7 @@ public class Car {
     public String getModel() {
         return model;
     }
+
     public String getInformation(){
         return brand + " " + model;
     }
--- app/src/main/java/com/example/samochod/DetailsActivity.java
+++ app/src/main/java/com/example/samochod/DetailsActivity.java
@@ -1,6 +1,9 @@
 package com.example.samochod;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +16,34 @@ public class DetailsActivity extends AppCompatActivity {
 
         String brand = getIntent().getStringExtra("brand");
         String model = getIntent().getStringExtra("model");
-
         EditText brandTxt = findViewById(R.id.brandTxt);
         EditText modelTxt = findViewById(R.id.modelTxt);
-
         brandTxt.setText(brand);
         modelTxt.setText(model);
+
+        Button deleteBtn = findViewById(R.id.deleteBtn);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button update = findViewById(R.id.updateBtn);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button addNew = findViewById(R.id.addBtn);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailsActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
